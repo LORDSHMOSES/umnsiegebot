7b05af12e4f21808bba6c8f212012ee017c85b86
LORDSHMOSES <33501532+LORDSHMOSES@users.noreply.github.com>
2019-10-28 12:09:37 -0500

    Main

---
+++ bot.py
@@ -0,0 +1,33 @@
+import discord
+import stats
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+@client.event
+#prevents bot from reading it's own messages
+async def on_message(message):
+    if message.author == client.user:
+        return
+#Help command
+    if message.content.startswith('$HMO help'):
+        await message.channel.send('Welcome to SHMOBOT the R6S bot for the UMN Siege Discord Server! \n \
+The following commands will be implemented soon: \n \
+$HMO lookup [UserName] \n \
+')
+#Stat Lookup Command
+    if message.content.startswith('$HMO lookup'):
+        channel = message.channel   
+        print(message.content[12:])
+        name = message.content[12:]
+        player = stats.playerClass(name)
+        player.getPlayerID()
+        player.getTabData()
+        await message.channel.send()
+#yeg
+    if message.content.startswith('yeg'):
+            await message.channel.send('yeg')
+client.run('NjM4MTIxNzA3NTUwNTM5Nzc4.XbYIjA.QwK2eA2o1maQHs6sHgVxKSQeQgY')
+
